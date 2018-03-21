ef896f9a00ca974daa7e0781cbedc82ae1682c37
Sean Hinds <sthinds144@gmail.com>
2018-03-20 23:51:21 -0500

    middleware revert

--- GeoClimate_Capture/settings.py
+++ GeoClimate_Capture/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 )
 
-MIDDLEWARE = (
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
