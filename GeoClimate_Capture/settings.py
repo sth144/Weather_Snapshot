c219ab6150e2f62c09e0592d972dc0071ed57a5f
Sean Hinds <sthinds144@gmail.com>
2018-03-20 22:43:19 -0500

    Troubleshooting static files

--- GeoClimate_Capture/settings.py
+++ GeoClimate_Capture/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
